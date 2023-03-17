b377f49119703951dfcadbc4a6d190ae34b038f2
Danil <martykhin84@gmail.com>
2023-03-17 14:00:50 +0300

    DZ

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+void ShowNambers (int n)
+{
+    if (n>=1)
+    {
+        Console.WriteLine(n+ " ");
+        ShowNambers(n-1);
+    }
+}
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("-----");
+ShowNambers(n);
+
+
+// задание 2 Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+int SumNumbers (int N, int M)
+{
+    if (M == 0) return (N * (N + 1)) / 2;            
+    else if (N == 0) return (M * (M + 1)) / 2;       
+    else if (M == N) return M;                       
+    else if (M < N) return N + SumNumbers(M, N - 1); 
+    else return N + SumNumbers(M, N + 1);  
+}
+
+Console.Write("M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Result, S = {SumNumbers(M, N)}");
+Console.ReadLine();
